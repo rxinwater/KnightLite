9afe589efa8b3bc8cb427c2a7539ddcf8ef12793
rxinwater <ryanaltjekiddo123@gmail.com>
2026-06-30 21:55:47 +0200

    added rough sketch of tavern and layout

---
+++ KnightLite/Rooms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace KnightLite
+{
+    public abstract class Rooms()
+    {
+        private bool _hasEntered = false;
+        public Rooms(bool hasEntered)
+        {
+            _hasEntered = hasEntered;  
+        }
+
+        private bool _hasSpawnedBefore = false;
+
+        public void Tavern()
+        {
+            if(_hasSpawnedBefore == false)
+            {
+                Console.WriteLine("");
+            }
+            _hasSpawnedBefore = true;
+        }
+    }
+}
